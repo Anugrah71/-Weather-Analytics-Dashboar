08cf62a6835735c717bfb624b3ec6c3735a6bdc2
Anugrah <your-email@example.com>
2025-11-01 10:56:34 +0530

    add backend and frontend logic

---
+++ client/src/features/weather/weatherSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchWeather, fetchForecast } from "./weatherThunks";
+import { fetchModule } from "vite";
+
+const weatherSlice = createSlice({
+  name: "weather",
+  initialState: {
+    cities: [],
+    favorites: [],
+    unit: "celsius",
+    status: "idle",
+    error: null,
+  },
+  reducers: {
+    toggleUnit: (state) => {
+      state.unit = state.unit === "celsius" ? "fahrenheit" : "celsius";
+    },
+    addFavorite: (state, action) => {
+      if (!state.favorites.includes(action.payload))
+        state.favorites.push(action.payload);
+    },
+    removeFavorite: (state, action) => {
+      state.favorites = state.favorites.filter(
+        (city) => city !== action.payload
+      );
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWeather.pending, (state) => {
+        state.state = "loading";
+      })
+      .addCase(fetchWeather.fullfilled, (state, action) => {
+        state.status = "succeeded";
+        const city = action.payload.location.name;
+        const existing = state.cities.find((c) => c.name == city);
+        if (existing) existing.data = action.payload;
+        else state.cities.push({ name: city, data: action.payload });
+      })
+      .addCase(fetchWeather.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { toggleUnit, addFavorite, removeFavorite } = weatherSlice.action;
+export default weatherSlice.reducer;
---
+++ client/src/features/weather/weatherThunks.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { getCurrentWeather, getForecast } from "../../api/weatherApi";
+
+export const fetchWeather = createAsyncThunk(
+  "weather/fetchWeather",
+  async (city) => {
+    const data = await getCurrentWeather(city);
+    return data;
+  }
+);
+export const fetchForecast = createAsyncThunk(
+  "weather/fetchForecast",
+  async (city) => {
+    const data = await getForecast(city);
+    return data;
+  }
+);
