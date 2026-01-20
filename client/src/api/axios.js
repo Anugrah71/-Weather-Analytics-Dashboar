ecf4edbb7b27ecd1650be785a9a3abb88e9fe827
ANUGRAH KARIYATT <162654910+Anugrah71@users.noreply.github.com>
2026-01-20 17:19:32 +0530

    Change API base URL to production endpoint

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://weather-analytics-dashboar.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
